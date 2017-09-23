9490aa13bbc84b83b1d96ac0cf839f3c3d5eed26
Jay Politzer <jmpolitzer@gmail.com>
2017-09-23 10:22:43 -0500

    adds more setup to server

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import path from 'path';
+import http from 'http';
+// import db from './server/db';
+
+const app = express();
+const port = parseInt(process.env.PORT, 10) || 8080;
+
+app.set('port', port);
+
+app.use(express.static(path.join(__dirname, '/../client/build')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+let count = 0;
+
+app.get('/increment', (req, res) => {
+  count++;
+
+  res.json({ count: count });
+});
+
+app.get('/decrement', (req, res) => {
+  count--;
+
+  res.json({ count: count });
+});
+
+// db.authenticate()
+// .then(() => {
+//   console.log('Successfully connected to db.');
+// })
+// .catch((err) => {
+//   console.error('Unable to connect to the database.', err);
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
