4f331132feba89b1365e3daa53e030f94eab0b6d
Jay Politzer <jmpolitzer@gmail.com>
2017-09-23 16:30:53 -0500

    adds todos CRUD

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = parseInt(process.env.PORT, 10) || 8080;
 
 app.set('port', port);
 
-app.use(express.static(path.join(__dirname, '/../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
@@ -28,8 +28,16 @@ app.get('/decrement', (req, res) => {
   res.json({ count: count });
 });
 
+require('./server/routes')(app);
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+  /*
+    TODO: Add 404 page.
+    res.sendFile(path.join(__dirname, 'client/build', '404.html'));
+  */
+  res.status(200).send({
+    message: 'Hmpf. It does\'t look like there\'s anything here.'
+  })
 });
 
 const server = http.createServer(app);
