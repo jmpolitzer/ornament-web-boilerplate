8e6e960c270126f4d22ef2cff2171b430ab3e712
Jay Politzer <jmpolitzer@gmail.com>
2017-09-30 08:28:43 -0500

    removes old counter code

--- client/src/redux/counter/actionTypes.js
+++
@@ -1,9 +0,0 @@
-/* Sagas watch for these actions... */
-export const INCREMENT_ASYNC = 'counter/INCREMENT_ASYNC';
-export const DECREMENT_ASYNC = 'counter/DECREMENT_ASYNC';
-
-/* ...and the reducer handles these. */ 
-export const INCREMENT_REQUESTED = 'counter/INCREMENT_REQUESTED';
-export const INCREMENT = 'counter/INCREMENT';
-export const DECREMENT_REQUESTED = 'counter/DECREMENT_REQUESTED';
-export const DECREMENT = 'counter/DECREMENT';
