4ef1fafbeb0af5d611bac2b49d366d9d4be6d316
Jay Politzer <jmpolitzer@gmail.com>
2017-09-30 08:23:54 -0500

    clears create todo form after submission

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -2,9 +2,19 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
 import todosReducer from './todos/reducer.js';
+import * as TodosConstants from './todos/constants';
 
 export default combineReducers({
   routing: routerReducer,
-  form: formReducer,
-  todos: todosReducer
+  todos: todosReducer,
+  form: formReducer.plugin({
+    createTodo: (state, action) => {
+      switch(action.type) {
+        case TodosConstants.CREATE_TODO_SUCCESS:
+          return undefined;
+        default:
+          return state;
+      }
+    }
+  })
 });
