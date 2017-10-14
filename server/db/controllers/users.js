ea8c919edcd481fe2a471e79e82070335ede20a5
Jay Politzer <jmpolitzer@gmail.com>
2017-10-14 17:58:02 -0500

    scopes api requests by user

--- server/db/controllers/users.js
+++ server/db/controllers/users.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models').User;
 
+/* TODO: Change out JWT secret key. */
+
 module.exports = {
   register(req, res) {
     return User.create({
@@ -33,7 +35,9 @@ module.exports = {
   },
 
   loginRequired(req, res, next) {
-    if(req.user) {
+    const user = jwt.verify(req.headers.cookie.split('=')[1], 'RESTFULAPIs');
+
+    if(req.user.id === user.id) {
       next();
     } else {
       return res.status(401).json({ message: 'Unauthorized user.' });
